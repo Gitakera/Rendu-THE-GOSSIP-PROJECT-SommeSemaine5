a0e80dc31736047e8d6a63dafd1e089619d9aa8c
Gitakera <myandopro@gmail.com>
2019-02-21 14:29:41 +0300

    correction de quelque bug

--- app/controllers/acceul_controller.rb
+++ app/controllers/acceul_controller.rb
@@ -1,23 +1,19 @@
 class AcceulController < ApplicationController
   def acceul
   	if(User.find_by(id: session[:user_id]))
-      actionok
-      
-    else
-      
-      redirect_to "/login"
-    end
-
-
-    def actionok 
-     puts "======================================================================================"	
+       puts "======================================================================================"  
      puts params
      a = Random.new
      Gossip.create(title: params[:titre], content: params[:contenu], user_id: session[:user_id] )
 
      puts "======================================================================================"
      redirect_to ''
-   end
+      
+    else
+      
+      redirect_to "/login"
+    end
+
  end
  
 end
